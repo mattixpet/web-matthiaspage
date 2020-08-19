ca59528ac07a38b22339f310982505799faa0877
Matthias Petursson <matthias01123@gmail.com>
2020-08-19 14:20:23 +0000

    Fix js console msg about dungeon link not found.

--- js/dungeon.js
+++ js/dungeon.js
@@ -4,18 +4,21 @@
 
 // javascript to light up the dungeon nav item on enter the dungeon click
 
-document.getElementById('dungeon-link').addEventListener('click', function(){
-	// toggle red border/ blink
-	var id = setInterval(function(){
-		document.getElementById('dungeon-nav').classList.toggle('itemFlashing');
-	}, 350);
+var dungeonLink = document.getElementById('dungeon-link');
+if (dungeonLink) {
+	dungeonLink.addEventListener('click', function(){
+		// toggle red border/ blink
+		var id = setInterval(function(){
+			document.getElementById('dungeon-nav').classList.toggle('itemFlashing');
+		}, 350);
 
-	// stop blinking
-	setTimeout(function(){
-		clearInterval(id);
-		document.getElementById('dungeon-nav').classList.remove('itemFlashing');
-	}, 4500);
-});
+		// stop blinking
+		setTimeout(function(){
+			clearInterval(id);
+			document.getElementById('dungeon-nav').classList.remove('itemFlashing');
+		}, 4500);
+	});
+}
 
 })();
 
