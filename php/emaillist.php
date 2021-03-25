0f41ec55ece8ba88d2d062740a09efe7db8fcfd6
Matthias Petursson <matthias01123@gmail.com>
2021-03-25 12:51:08 +0000

    Add further spam protection, spambot must answer one question.

--- php/emaillist.php
+++ php/emaillist.php
@@ -1,5 +1,22 @@
 <?php
 
+  // User has to solve this before email is submitted
+  $spamprotectionhtml = '
+  <!doctype html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <form action="/php/emaillist.php?gnaften=SUBMITTEDEMAIL" method="post" id="emaillist_form">
+      <label for="answer">What\'s the first letter of the word Eyjafjallajökull?</label>
+      <input type="text" id="answer" name="answer" size="18" required>
+      <button type="submit" id="emaillist_button">Submit!</button>
+    </form>
+  </body>
+  </html>
+  ';
+
   $errors = array();
   try {
     $conn = new PDO(/* here is some private information about connecting to the database! */);
@@ -14,21 +31,35 @@
     if ($method === 'POST') {
       // gnaften = netfang backwards, spam protection
       $email = $_POST['gnaften'];
+      if (!$email) {
+        // On spam validation email is sent as url variable, get it here
+        $email = $_GET['gnaften'];
+      }      
+
       if ($email) {
         if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-          $results = array();
+          // For spam protection, have user answer a question, answer is 'E' or 'e'
+          // (what's the first letter of Eyjafjallajökull)
+          $answer = $_POST['answer'];
+          if ($answer && ($answer == 'e' || $answer == 'E')) {
+            // Passed the question, correct answer
+            $results = array();
 
-          $query = $conn->prepare(
-            'INSERT INTO emails (email) ' .
-            'VALUES (:email) ' .
-            'ON DUPLICATE KEY UPDATE id = id'
-          );
-          $results[] = $query->execute(array('email' => $email));
+            $query = $conn->prepare(
+              'INSERT INTO emails (email) ' .
+              'VALUES (:email) ' .
+              'ON DUPLICATE KEY UPDATE id = id'
+            );
+            $results[] = $query->execute(array('email' => $email));
 
-          if (sizeof($results) === 0) {
-            echo 'Failed to add email! Sorry try again, or just contact me directly, I\'ll add you to the list. <a href="/">Back</a>';
+            if (sizeof($results) === 0) {
+              echo 'Failed to add email! Sorry try again, or just contact me directly, I\'ll add you to the list. <a href="/">Back</a>';
+            } else {
+              echo 'Thank you! You have been added to my email list! :D <a href="/">Back</a>';
+            }
           } else {
-            echo 'Thank you! You have been added to my email list! :D <a href="/">Back</a>';
+            // Wrong answer or first time submitting email, so prompt for verification
+            echo str_replace('SUBMITTEDEMAIL', $email, $spamprotectionhtml);
           }
         } else {
           $query = $conn->prepare(
