d8a4e53cf7f0faa7c05a82ac80f8ceb1a76c70d1
Matthias Petursson <matthias01123@gmail.com>
2023-05-15 18:59:02 +0000

    Sending email uses google api now.

--- _python/sendEmailToList.py
+++ _python/sendEmailToList.py
@@ -4,16 +4,43 @@
 
 # Needs files subject.txt, content.txt (email body), emaillist.txt and emaillist_code.txt
 
-import smtplib
+import os.path
+import hashlib
+from time import sleep
 
+import base64
+from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-import hashlib
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-from time import sleep
+SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
 
 def main():
+	# https://developers.google.com/gmail/api/quickstart/python
+	creds = None
+	# The file token.json stores the user's access and refresh tokens, and is
+	# created automatically when the authorization flow completes for the first
+	# time.
+	if os.path.exists('token.json'):
+		creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+	# If there are no (valid) credentials available, let the user log in.
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file(
+				'credentials.json', SCOPES)
+			creds = flow.run_local_server(port=0)
+		# Save the credentials for the next run
+		with open('token.json', 'w') as token:
+			token.write(creds.to_json())
+
 	subject = '' # just one line, txt
 	with open('subject.txt', 'r', encoding="utf-8") as f:
 		subject = f.read()
@@ -21,13 +48,6 @@ def main():
 	content = '' # html
 	with open('content.txt', 'r', encoding="utf-8") as f:
 		content = f.read()
-
-	# connect to email server
-	server = smtplib.SMTP('smtp.gmail.com', 587)
-	server.ehlo()
-	server.starttls()
-	server.ehlo()
-	server.login("matthias.crazy.email.bot@gmail.com", "my super secret password !")
 
 	# get list of emails from this file
 	emaillist = []
@@ -52,9 +72,22 @@ def main():
 		msg.attach(MIMEText(content.format(email=email, emailHash=emailHash), 'html'))
 		text = msg.as_string()
 
-		# send mail
+		# send mail through the Gmail API
 		print('Sending email to: {}'.format(email))
-		server.sendmail(fromaddr, email, text)
+		service = build('gmail', 'v1', credentials=creds)
+		
+		encoded_message = base64.urlsafe_b64encode(msg.as_bytes()).decode()
+
+		create_message = {
+			'raw': encoded_message
+		}
+
+		send_message = (
+			service.users().messages().send(
+				userId="me",
+				body=create_message
+			).execute()
+		)
 
 		# wait a bit so as to not send the emails too rapidly
 		sleep(1)
